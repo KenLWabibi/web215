ed2aef9236b200aacc88e4e7885d8472d9de8a56
Ken Wabibi <157647708+KenLWabibi@users.noreply.github.com>
2025-11-02 14:24:23 -0500

    Update index.js

--- mern/server/index.js
+++ mern/server/index.js
@@ -6,10 +6,12 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
-})
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 
 const port = process.env.PORT || 4000;
 
