36b02115ca06d5040bef10e54a4b3556e9852c76
Ken Wabibi <157647708+KenLWabibi@users.noreply.github.com>
2025-11-04 01:20:46 -0500

    Update builder.sh

--- mern/builder.sh
+++ mern/builder.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# Navigate to the server folder
+
 cd "$(dirname "$0")/server" || { 
     echo "Error: server directory not found"; 
     exit 1; 
 }
 
-# Install dependencies
+
 npm install
 
-# Build or start server if needed
-# Uncomment one of these depending on your setup
-# npm run build
-# npm start
