cb0b60d580c41a01ce628c5f4718b15f76e4d26a
Ken Wabibi <157647708+KenLWabibi@users.noreply.github.com>
2025-11-04 05:04:14 -0500

    Update index.js
    Added CORS middleware

--- mern/server/index.js
+++ mern/server/index.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors"); // <-- import cors
 const { connectToMongoDB } = require("./database");
 
 const app = express();
 app.use(express.json());
 
+// CORS middleware
+app.use(cors({
+  origin: "https://awesome-todos-app-e8hh.onrender.com", 
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
 // Load API routes 
 const router = require("./routes");
 app.use("/api", router);
-
 
 app.get("/", (req, res) => {
   res.send("Backend server is running!");
