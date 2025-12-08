fd086487610cb4bcd201c5a5e790bdc0817b91a7
Ken Wabibi <kenwabibi@Kens-MBP.lan>
2025-12-08 01:23:57 -0500

    updated app.use and app.get  __dirname

--- belpshare/server/server.js
+++ belpshare/server/server.js
@@ -43,9 +43,9 @@ app.post("/logout", (req, res) => {
   res.json({ success: true });
 });
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 
 
