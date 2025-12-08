762180d8c0b18d1b7a3bb0a1157e5ceaf07a5d10
Ken Wabibi <kenwabibi@Kens-MBP.lan>
2025-12-08 01:49:40 -0500

    app.get to use a catch-all

--- belpshare/server/server.js
+++ belpshare/server/server.js
@@ -47,7 +47,7 @@ const router = require("./routes");
 app.use("/api", router);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("*", (req, res) => {
+app.get("/^\/.*$/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 
